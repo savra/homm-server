8e0f69586d1206ba22b944fe3c3352b289c3ed57
Sergey Savrukhin <savrawar@mail.ru>
2021-05-29 18:36:10 +0300

    Initial commit

---
+++ src/test/java/com/hvdbs/hommserver/HommServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvdbs.hommserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HommServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
